555efed1fdaf153eca41ba9cc71c034c91c62730
Erkinai <101646157+ErkinaiAKZ@users.noreply.github.com>
2022-04-09 20:45:58 +0600

    Add files via upload

---
+++ integers.py
@@ -0,0 +1,101 @@
+#lesson2
+
+#problem 1:
+
+print(17925 >= 34**2)
+print(17925 >= 26*3)
+print(17925 >= 17*33)
+print(17925 >= 4394*4)
+
+#problem 2:
+#1.
+
+a = 1
+b = 2
+c = 3
+print(c>a>b)
+
+#2.
+
+num = 7 % 3 * 4.8
+print(num)
+
+#3.
+
+print(c+8 == 10+1)
+
+#4.
+
+print(7-2 != 3+3)
+
+#problem 3:
+
+pr1 = 255 + 4535
+pr2 = pr1 * 39
+pr3 = pr2 ** 2
+pr4 = pr3 - 193432
+print(pr1, pr2, pr3, pr4)
+
+#problem 4:
+
+a = 17 * 3
+b = 12 * 5
+c = 12 ** 3
+d = 13 * 7
+e = 4 ** 5
+f = 512 + 512
+print(a, b, c, d, e, f)
+print('The biggest:', c)
+
+#problem 5:
+
+print(-21//10)
+#результат целочисленного деления округляется в меньшую сторону
+
+#problem 6:
+
+num = (28 / 75) * 100
+print(num, 'percents')
+
+#problem 7:
+
+a = 1994
+b = 2022
+print((b-a)+2)
+print((b-a)-2)
+
+#problem 8:
+
+num=(25 + 75 + 10 + 95) / 4
+print(num)
+
+#problem 9:
+
+a = 5.7
+b = 5.7
+c = 5.7 
+z = (a + b) * c
+print(z)
+
+#problem 10:
+
+a = 4
+print(a)
+b = 10
+print(b)
+c = 6.3
+print(c)
+d = 3
+print(d)
+e = 8
+print(e)
+
+#problem 11:
+
+a = 4 
+b = 10
+c = 6.3
+d = 3
+e = 8
+z = (a-e ** (b / d)) % c
+print(z)
